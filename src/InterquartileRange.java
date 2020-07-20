9a8115c1a9ec049fe319441f33b9435341e13bd7
Austin Oyugi <austinoyugi@gmail.com>
2020-07-20 17:50:51 +0300

    Create InterquartileRange.java

---
+++ src/InterquartileRange.java
@@ -0,0 +1,64 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
+/**
+ * @author Austin Oyugi
+ * @date 7/20/2020
+ * @email austinoyugi@gmail.com
+ */
+public class InterquartileRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+
+        List<Integer> elements = new ArrayList<>();
+        List<Integer> integerList = new ArrayList<Integer>(){
+            public boolean add(Integer integer) {
+                int index = Collections.binarySearch(this, integer);
+                if (index < 0) index = ~index;
+                super.add(index, integer);
+                return true;
+            }
+        };
+
+        for (int i = 0; i<numberOfElements; i++){
+            elements.add(scanner.nextInt());
+        }
+
+        for (int i =0 ; i<numberOfElements; i++){
+            int frequency = scanner.nextInt();
+            for (int x =0; x<frequency; x++)
+                integerList.add(elements.get(i));
+        }
+
+        double median = getMedian(integerList);
+        List<Integer> lowerQuartiles = new ArrayList<>();
+        List<Integer> upperQuartiles = new ArrayList<>();
+
+        integerList.forEach(x->{
+            if (x<median)
+                lowerQuartiles.add(x);
+            else if (x>median)
+                upperQuartiles.add(x);
+        });
+
+        double lowerQuartile = getMedian(lowerQuartiles);
+        double uppperQuartile = getMedian(upperQuartiles);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.0");
+        System.out.println(decimalFormat.format(uppperQuartile - lowerQuartile));
+        System.out.println((int) lowerQuartile);
+        System.out.println((int) median);
+        System.out.println((int)uppperQuartile);
+    }
+
+    public static double getMedian(List<Integer> theArray) {
+        int position = theArray.size()/2;
+        double median;
+        if (theArray.size()%2 == 0)
+            median = (double) (theArray.get(position-1) + theArray.get(position))/2;
+        else
+            median = (double) theArray.get((int) (Math.ceil((double) theArray.size()/2))-1);
+        return (median);
+    }
+}
